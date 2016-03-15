b3a86cc3b9281092b9d8360723e83b414e506207
yukifrog <c9@yukifrog.com>
2016-03-15 16:34:27 +0900

    Finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -9,4 +10,29 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, options = {})
+    password    = options[:password]    || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email:       user.email,
+                                  password:    password,
+                                  remember_me: remember_me }
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+    # 統合テスト内ではtrueを返す
+    def integration_test?
+      defined?(post_via_redirect)
+    end
+
 end
